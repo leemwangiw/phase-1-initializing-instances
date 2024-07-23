3126d1331c22aef65b9caf7807e4bd0d2915e4dd
leemwangiw <lee.mwangi1@student.moringaschool.com>
2024-07-23 10:54:48 +0300

    commit all

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+// Define the Breakfast class
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Define the Lunch class
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  // Define the Dinner class
+  class Dinner {
+    #dessert; // private field
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert;
+    }
+  }
+  
+  module.exports = { Breakfast, Lunch, Dinner };
+  
